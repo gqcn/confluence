9ca8513f7587600deb459900162c995dfd443ebb
john <john@johng.cn>
2020-12-22 10:11:51 +0800

    up

---
+++ syntaxhighlighter/shBrushIni.js
@@ -0,0 +1,5 @@
+SyntaxHighlighter.brushes.Ini = function() {
+    this.regexList = [];
+};
+SyntaxHighlighter.brushes.Ini.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Ini.aliases   = ['ini'];
---
+++ syntaxhighlighter/shBrushToml.js
@@ -0,0 +1,5 @@
+SyntaxHighlighter.brushes.Toml = function() {
+    this.regexList = [];
+};
+SyntaxHighlighter.brushes.Toml.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Toml.aliases   = ['toml'];
