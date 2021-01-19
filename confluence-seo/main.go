342eecb2ba5d235776195b25bd7232a6f462594a
john <john@johng.cn>
2021-01-20 00:14:27 +0800

    up

--- confluence-seo/main.go
+++ confluence-seo/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"github.com/gogf/gf/container/garray"
 	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/os/gcron"
+	"github.com/gogf/gf/os/gfile"
 )
 
-func main() {
+func makeBaiduSiteMap() {
 	sql := `
 SELECT DISTINCT CONTENTID FROM CONTENT 
 WHERE CONTENTTYPE='PAGE' and CONTENT_STATUS='current'  and SPACEID > 0
@@ -19,9 +21,30 @@ WHERE CONTENTTYPE='PAGE' and CONTENT_STATUS='current'  and SPACEID > 0
 	for _, item := range list {
 		urlArray.Append(fmt.Sprintf(`https://itician.org/pages/viewpage.action?pageId=%d`, item["CONTENTID"].Int()))
 	}
-	response := g.Client().ContentType(`text/plain`).PostContent(
+	gfile.PutContents(gfile.Join(g.Cfg().GetString(`setting.sitemap`), "sitemap.txt"), urlArray.Join("\n"))
+}
+
+func makeBaiduApiRequest() {
+	sql := `
+SELECT DISTINCT CONTENTID FROM CONTENT 
+WHERE CONTENTTYPE='PAGE' and CONTENT_STATUS='current' and SPACEID > 0
+`
+	list, err := g.DB().GetAll(sql)
+	if err != nil {
+		panic(err)
+	}
+	urlArray := garray.NewStrArray()
+	for _, item := range list {
+		urlArray.Append(fmt.Sprintf(`https://itician.org/pages/viewpage.action?pageId=%d`, item["CONTENTID"].Int()))
+	}
+	g.Client().ContentType(`text/plain`).PostContent(
 		`http://data.zz.baidu.com/urls?site=https://itician.org&token=`+g.Cfg().GetString("baidu.ziyuan.token"),
 		urlArray.Join("\n"),
 	)
-	fmt.Println(response)
+}
+
+func main() {
+	gcron.Add(`0 0 0 * * *`, makeBaiduSiteMap)
+	gcron.Add(`0 0 0 * * *`, makeBaiduApiRequest)
+	select {}
 }
