3480c3101fdefe2470a9532c1416c540e07f9f4f
txqiangguo <txqiangguo@tencent.com>
2023-03-14 21:07:40 +0800

    up

---
+++ confluence/syntaxhighlighter/shBrushProtobuf.js
@@ -0,0 +1,5 @@
+SyntaxHighlighter.brushes.Protobuf = function() {
+    this.regexList = [];
+};
+SyntaxHighlighter.brushes.Protobuf.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Protobuf.aliases   = ['protobuf'];
